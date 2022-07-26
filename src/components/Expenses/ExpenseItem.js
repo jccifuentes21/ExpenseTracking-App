de0b4a6ae2ac9a9c3ee374f7968b0d295a54e2c5
Juan Camilo <jccifuentes21@gmail.com>
2022-07-25 21:58:04 -0700

    updated folder name

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 
 import ExpenseDate from './ExpenseDate';
@@ -7,23 +7,16 @@ import './ExpenseItem.css'
 
 const ExpenseItem = (props) =>{
 
-  const [title, setTitle] = useState(props.title);
-
-
-  const clickHander = () =>{
-    setTitle('Updated!');
-    console.log('clicked!')
-  }
-
   return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date}/>
-      <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-      </div>
-      <button onClick={clickHander}>Change title</button>
-    </Card>
+    <li>
+      <Card className='expense-item'>
+        <ExpenseDate date={props.date}/>
+        <div className='expense-item__description'>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
   
 }
